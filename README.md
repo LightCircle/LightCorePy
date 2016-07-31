db8f1e6205dc254d0142d2b1a7ab4931ce7ae2d9
LI LIN <r2space@gmail.com>
2016-07-31 19:08:47 +0800

    添加基于Mongoldb的Session的支持

--- README.md
+++ README.md
@@ -4,3 +4,17 @@
  参考 http://monklof.com/post/19/
 
 
+#### TODO
+1. Web Framework 选择 Flask Django Bottle
+
+2. 解决动态路由
+ api
+ web页面
+
+3. LightAdmin数据API化
+ 允许Python启动Admin
+
+4. OR Mapping
+ 与MongoPy的结合
+
+5. Session的DB话
--- light/main.py
+++ light/main.py
@@ -2,6 +2,7 @@ from flask import Flask
 from functools import partial
 import importlib.util
 import os
+import light.mongo.session
 
 app = Flask(__name__)
 
@@ -20,6 +21,7 @@ func1 = partial(ctrl.hello, page='hello')
 func2 = partial(ctrl.static)
 func3 = partial(ctrl.test, page='hello')
 func4 = partial(ctrl.upload)
+func5 = partial(ctrl.download)
 
 
 # 绑定URL与CTRL的方法
@@ -27,6 +29,7 @@ def bind():
     app.add_url_rule('/{0}'.format('hello'), endpoint='hello', view_func=func1, methods=['GET'])
     app.add_url_rule('/upload', endpoint='mmm', view_func=func3, methods=['GET'])
     app.add_url_rule('/upload', endpoint='nnn', view_func=func4, methods=['POST'])
+    app.add_url_rule('/download', endpoint='qqq', view_func=func5, methods=['GET'])
 
 
 # 静态资源解析
@@ -34,7 +37,13 @@ def bind_static():
     app.add_url_rule('/js/<path:path>', endpoint='sss', view_func=func2)
 
 
+# 初始化基于Mongo的Session
+def session():
+    app.session_interface = light.mongo.session.MongoSessionInterface(db='python', port=57017, host='localhost')
+
+
 if __name__ == "__main__":
+    session()
     bind()
     bind_static()
     app.run()

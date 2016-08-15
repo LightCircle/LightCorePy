4c4aef28672175535ea336884a805327f151b455
LI LIN <r2space@gmail.com>
2016-08-15 14:36:40 +0800

    添加upload例子

--- light/model/file.py
+++ light/model/file.py
@@ -1,3 +1,7 @@
+import flask
+import os
+
+
 def add():
     raise NotImplementedError
 
@@ -7,6 +11,35 @@ def update():
 
 
 def upload():
+
+    # <form action="" method=post enctype=multipart/form-data>
+    # <p>
+    #     <input type=file name=file>
+    #     <input type=submit value=upload>
+    # </p>
+    # </form>
+
+    # app.add_url_rule('/upload', endpoint='nnn', view_func=func4, methods=['POST'])
+    # app.add_url_rule('/download', endpoint='qqq', view_func=func5, methods=['GET'])
+
+    # 保存上传文件
+    def upload():
+        if flask.request.method == 'POST':
+            file = flask.request.files['file']
+            d = os.path.join(os.path.abspath('..'), 'uploaded')
+            file.save(d)
+
+        return 'OK'
+
+    # 对应下载文件
+    def download():
+        def generate():
+            data = [['1', '2', '3', '4', '5'], ['6', '7', '8', '9', '10']]
+            for row in data:
+                yield ','.join(row) + '\n'
+
+        return flask.Response(generate(), mimetype='text/csv')
+
     raise NotImplementedError
 
 

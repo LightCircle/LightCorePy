1c33c040268013d2477e25f90740f8e3d3358d52
LI LIN <r2space@gmail.com>
2016-10-09 21:49:54 +0800

    添加 db 操作的 validator 方法

--- light/validator.py
+++ light/validator.py
@@ -1,5 +1,10 @@
 import jmespath
 
+from light.mongo.model import Model
+from light.constant import Const
+
+CONST = Const()
+
 
 class Validator(object):
     def __init__(self, handler):
@@ -12,8 +17,9 @@ class Validator(object):
 
         rule = method[0]['rule']
         data = jmespath.search(method[0]['key'], {'data': self.handler.params.data})
+        option = method[0]['option']
 
-        result = getattr(Rule(), rule)(data)
+        result = getattr(Rule(), rule)(self.handler, data, option)
         if not result:
             return method[0]['message']
 
@@ -23,7 +29,7 @@ class Rule(object):
         pass
 
     @staticmethod
-    def is_number(data):
+    def is_number(handler, data, option):
         print(data)
 
         if isinstance(data, int):
@@ -35,8 +41,14 @@ class Rule(object):
         return False
 
     @staticmethod
-    def is_string(data):
+    def is_string(handler, data, option):
         if isinstance(data, str):
             return True
 
         return False
+
+    @staticmethod
+    def is_unique(handler, data, option):
+        model = Model(domain=handler.domain, code=handler.code, table=option['table'])
+        count = model.total(condition=option['condition'])
+        return count <= 0
--- tests/test_validator.py
+++ tests/test_validator.py
@@ -1,7 +1,11 @@
+import os
 import unittest
 
 from light.validator import Validator
 from light.http.context import Context
+from light.constant import Const
+
+CONST = Const()
 
 
 class TestValidator(unittest.TestCase):
@@ -10,13 +14,16 @@ class TestValidator(unittest.TestCase):
         result = self.validator.is_valid(['a1'], self.validation)
         print(result)
 
-        # self.assertTrue(validator.is_number(1))
-        # self.assertFalse(validator.is_number('1'))
-        # self.assertTrue(validator.is_number(1.0))
-        # self.assertFalse(validator.is_number(None))
-        # self.assertFalse(validator.is_number(True))
+        result = self.validator.is_valid(['be49d0f4'], self.validation)
+        print(result)
 
     def setUp(self):
+
+        os.environ[CONST.ENV_LIGHT_DB_HOST] = 'db.alphabets.cn'
+        os.environ[CONST.ENV_LIGHT_DB_PORT] = '57017'
+        os.environ[CONST.ENV_LIGHT_DB_USER] = 'light'
+        os.environ[CONST.ENV_LIGHT_DB_PASS] = '2e35501c2b7e'
+
         self.handler = Context(uid='000000000000000000000001', domain='LightDB', code='light', param={'data': {}})
         self.validator = Validator(self.handler)
         self.validation = [
@@ -25,26 +32,20 @@ class TestValidator(unittest.TestCase):
                 'name': 'a1',
                 'rule': 'is_number',
                 'key': 'data.age',
-                'message': 'a1 not correct'
+                'message': 'a1 not correct',
+                'option': []
             },
             {
-                'group': '/api/access/add',
+                'group': 'unique_test',
                 'name': 'be49d0f4',
-                'rule': 'range',
-                'key': 'data.password',
-                'option': ['4', '16'],
-                'message': '密码长度需要在4-16位'
-            },
-            {
-                'group': '',
-                'name': 'staff_check_10',
-                'rule': 'exists',
-                'key': 'data.project',
+                'rule': 'is_unique',
+                'key': 'data.id',
                 'option': {
-                    'table': 'group',
-                    'conditions': {
-                        '_id': '$data.project'
+                    'table': 'user',
+                    'condition': {
+                        'id': 'sloan'
                     }
-                }
+                },
+                'message': '用户名已经存在'
             }
         ]

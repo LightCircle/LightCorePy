b23abc3b413b360094677f79ced0bf55a4c09405
LI LIN <r2space@gmail.com>
2017-04-01 09:33:58 +0800

    添加 500 错误

--- light/error/http.py
+++ light/error/http.py
@@ -2,3 +2,9 @@ class BadRequest(Exception):
     def __init__(self):
         self.code = 400
         self.message = 'Bad Request'
+
+
+class InternalServerError(Exception):
+    def __init__(self):
+        self.code = 500
+        self.message = 'Internal Server Error'
